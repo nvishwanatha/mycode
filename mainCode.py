b4351e606ad30eaad498533301c9745a710e6622
nvishwanatha <n.vishwanatha@gmail.com>
2022-07-04 18:19:57 -0700

    KeyWords

---
+++ mainCode.py
@@ -0,0 +1,30 @@
+# Python program to illustrate
+# function with main
+import output as output
+
+
+def getInteger():
+    result = int(input("Enter integer: "))
+    return result
+
+
+def Main():
+    print("Started")
+
+    # calling the getInteger function and
+    # storing its returned value in the output variable
+    output = getInteger()
+    print(output)
+
+
+  # Python program to illustrate
+    # a simple for loop
+
+    for step in range(output):
+        print(step)
+
+# now we are required to tell Python
+# for 'Main' function existence
+if __name__ == "__main__":
+    Main()
+
